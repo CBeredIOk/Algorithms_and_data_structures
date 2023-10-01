ea27843e3accdd847e1c67d1214e845a373af685
CBeredIOk <CBeredIOk@yandex.ru>
2023-10-01 11:22:27 +0300

    Add tasks 1, 2, 3 from chapter 10

---
+++ head_10/DataItem.java
@@ -0,0 +1,13 @@
+package Lafore.head_10;
+
+class DataItem
+{
+    public long dData; // Один объект данных
+
+    public DataItem(long dd) // Конструктор
+    { dData = dd; }
+
+    public void displayItem() // Вывод элемента в формате "/27"
+    { System.out.print("/"+dData); }
+}
+
---
+++ head_10/Tree234App.java
@@ -0,0 +1,84 @@
+package Lafore.head_10;
+
+import java.io.*;
+import java.util.Arrays;
+
+class Tree234App
+{
+    public static void main(String[] args) throws IOException
+    {
+        long value;
+        Tree234 theTree = new Tree234();
+
+        while(true)
+        {
+            System.out.print("Enter first letter of ");
+            System.out.print("show, insert, or find: ");
+            char choice = getChar();
+            switch(choice)
+            {
+                case 'S':
+                    theTree.displayTree();
+                    break;
+                case 'a':
+                    theTree.insert(50);
+                    theTree.insert(40);
+                    theTree.insert(60);
+                    theTree.insert(30);
+                    theTree.insert(70);
+                case 's':
+                    System.out.print("Enter length array: ");
+                    value = getInt();
+                    long[] arrayLong = new long[(int) value];
+                    System.out.println("Enter array: ");
+                    for (int i = 0; i < value; i++){
+                        arrayLong[i] = getInt();
+                    }
+                    System.out.println(Arrays.toString(arrayLong));
+                    System.out.println(Arrays.toString(theTree.sort(arrayLong)));
+                    break;
+                case 'i':
+                    System.out.print("Enter value to insert: ");
+                    value = getInt();
+                    theTree.insert(value);
+                    break;
+                case 'f':
+                    System.out.print("Enter value to find: ");
+                    value = getInt();
+                    int found = theTree.find(value);
+                    if(found != -1)
+                        System.out.println("Found "+value);
+                    else
+                        System.out.println("Could not find "+value);
+                    break;
+                case 'm':
+                    long minElem = theTree.findMinimum();
+                    System.out.println("Minimum is " + minElem);
+                case 't':
+                    theTree.symmetricTraversal();
+                default:
+                    System.out.print("Invalid entry\n");
+            }
+        }
+    }
+
+    public static String getString() throws IOException
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+
+    public static char getChar() throws IOException
+    {
+        String s = getString();
+        return s.charAt(0);
+    }
+
+    public static int getInt() throws IOException
+    {
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+}
